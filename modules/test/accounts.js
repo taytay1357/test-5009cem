43642c3b2fdeeaab4c1bdd71ed69dc9dc0ddd056
Josh Taylor <taylorj12@coventry.ac.uk>
2022-03-11 20:13:11 +0000

    doing some UAT testing and finalising unit tests

--- modules/test/accounts.js
+++ modules/test/accounts.js
@@ -1,7 +1,11 @@
-// export async function login(data) {
+import { db } from './db.js'
 
-// }
-
-// export async function register(data){
+export async function login(data) {
+    // ARRANGE
+    // FUNCTION USES SELECT TO GET DATA FROM DB.js
     
-// }
+}
+
+export async function register(data){
+    
+}
---
+++ modules/test/db.js
@@ -0,0 +1,7 @@
+const db = {
+    accounts : {user: "user1", pass: "p455w0rd", id: 1},
+    stock: {isbn: 1212, title: "1984" , author: "George Orwell", publication_date: "19/08/1972", }
+
+}
+
+export { db }
