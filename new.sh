ee53c9dce8926b2bcfcb2abeca8c97dd56bbfa37
Josh Taylor <taylorj12@coventry.ac.uk>
2022-03-29 10:17:44 +0000

    changes to my testing shell script

--- new.sh
+++ new.sh
@@ -1,8 +1,10 @@
 echo "RUNNING TEST SUITE"
 
+rm -r -f ./coverage/
+rm -r -f ./cov_profile/
 deno test --unstable --allow-all ./modules --coverage=cov_profile
 deno coverage cov_profile --lcov > cov_profile.lcov
 genhtml -o ./coverage/html/ cov_profile.lcov
-rm ./cov_profile.lcov
+rm -r -f ./cov_profile/
 
 echo "COVERAGE REPORT PRODUCED AND LCOV REMOVED"
